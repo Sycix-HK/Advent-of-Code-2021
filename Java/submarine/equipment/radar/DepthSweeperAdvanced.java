f846ced85a646b52d0dd443648e8e40340e242ff
Kristóf Hoffmann <WarriorKatHun@gmail.com>
2021-12-02 16:57:58 +0100

    Fixed advanced sweeper

--- Java/submarine/equipment/radar/DepthSweeperAdvanced.java
+++ Java/submarine/equipment/radar/DepthSweeperAdvanced.java
@@ -5,17 +5,17 @@ import java.util.*;
 
 public class DepthSweeperAdvanced{
     public static int Sweep(File input) {
-        List<Integer> scan = new submarine.core.InputScanner<Integer>(input).getResult();
+        ArrayList<Integer> scan = new submarine.core.InputScannerInteger(input).getResult();
         
         // denoise
-        for (int i = 0; i < scan.size()-3; i++) {
-            scan.set(i, (scan.get(i)+scan.get(i+1)+scan.get(i+2)));
+        for (int i = 0; i < scan.size()-2; i++) {
+            scan.set(i, (scan.get(i) + scan.get(i+1) + scan.get(i+2)));
         }
 
         // count
         int c = 0;
-        for (int i = 0; i < scan.size()-3; i++) {
-            if (scan.get(i) > scan.get(i+1)) {c++;}
+        for (int i = 0; i < scan.size()-2; i++) {
+            if (scan.get(i) < scan.get(i+1)) {c++;}
         }
 
         return c;
