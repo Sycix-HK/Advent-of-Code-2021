42f1d11cfb54b02c0b720501a1014659f456c1ed
Kristóf Hoffmann <WarriorKatHun@gmail.com>
2021-12-02 16:58:16 +0100

    Created Main

--- Java/submarine/equipment/radar/DepthSweeperMain.java
+++ Java/submarine/equipment/radar/DepthSweeperMain.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class DepthSweeperMain {
     public static void main(String[] args) {
-        File input = new File(args[0]);
+        File input = new File("data/Day 01/input.txt");
 
         System.out.println("Simple scan: "+ DepthSweeperSimple.Sweep(input));
         System.out.println("Denoised scan: "+ DepthSweeperAdvanced.Sweep(input));
