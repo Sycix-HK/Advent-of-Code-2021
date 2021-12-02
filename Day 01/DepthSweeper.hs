99602c40a261010e18fa4b533d1081cdd728163a
Kristóf Hoffmann <WarriorKatHun@gmail.com>
2021-12-02 16:07:10 +0100

    moved

--- Day 01/DepthSweeper.hs
+++
@@ -1,8 +0,0 @@
-depthSweeper :: [Integer] -> Int
-depthSweeper input = sum $ zipWith (\x a -> if x < a then 1 else 0) input (tail input)
-
-denoiseData :: [Integer] -> [Integer]
-denoiseData (x:xs) = if length xs < 2 then [] else (x + xs!!0 + xs!!1) : denoiseData xs 
-
-depthSweeperDenoised :: [Integer] -> Int
-depthSweeperDenoised = depthSweeper . denoiseData
