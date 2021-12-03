a091256eb6728d7109b2d7021c5dbe830082d0a1
Kristóf Hoffmann <34631181+Sycix-HK@users.noreply.github.com>
2021-12-03 21:06:46 +0100

    Clarifying input assumption

--- Haskell/Day 02/PathCalculator.hs
+++ Haskell/Day 02/PathCalculator.hs
@@ -1,9 +1,12 @@
 data Direction = Forward Int | Up Int | Down Int deriving (Eq, Show)
 
+-- Assuming input comes as [Direction]
+
 -- Part 1
 calculatePathProductSimple :: [Direction] -> Int
 calculatePathProductSimple input = (sum [x | (Forward x) <- input]) * ((sum [x | (Down x) <- input]) - (sum [x | (Up x) <- input]))
 
+-- Part 2's ghost
 --calculatePathAdvanced :: [Direction] -> Int -> (Int,Int)
 --calculatePathAdvanced ((Forward x):xs) aim =  ()
 --calculatePathAdvanced ((Up x):xs)      aim =  
