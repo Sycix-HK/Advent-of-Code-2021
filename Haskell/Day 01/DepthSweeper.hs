7ec799eb47de14c6d8c778b6f4eae3e417a20d1e
Kristóf Hoffmann <WarriorKatHun@gmail.com>
2021-12-03 11:17:50 +0100

    Added comments

--- Haskell/Day 01/DepthSweeper.hs
+++ Haskell/Day 01/DepthSweeper.hs
@@ -1,6 +1,8 @@
+-- Part 1
 depthSweeper :: [Integer] -> Int
 depthSweeper input = sum $ zipWith (\x a -> if x < a then 1 else 0) input (tail input)
 
+-- Part 2
 denoiseData :: [Integer] -> [Integer]
 denoiseData (x:xs) = if length xs < 2 then [] else (x + xs!!0 + xs!!1) : denoiseData xs 
 
