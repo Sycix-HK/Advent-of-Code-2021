79b7be5544ecfd852cb958deeb1629348b324592
Kristóf Hoffmann <WarriorKatHun@gmail.com>
2021-12-05 11:47:42 +0100

    Solved Day 5 part 1

--- Java/README.md
+++ Java/README.md
@@ -6,6 +6,7 @@
 * [Day 02](https://github.com/Sycix-HK/Advent-of-Code-2021/tree/main/Java/submarine/equipment/propeller) <sup>⭐⭐</sup>
 * [Day 03](https://github.com/Sycix-HK/Advent-of-Code-2021/tree/main/Java/submarine/equipment/diagnostics) <sup>⭐⭐</sup>
 * [Day 04](https://github.com/Sycix-HK/Advent-of-Code-2021/tree/main/Java/submarine/equipment/entertainment) <sup>⭐⭐</sup>
+* [Day 05](https://github.com/Sycix-HK/Advent-of-Code-2021/tree/main/Java/submarine/equipment/sonar) <sup>⭐</sup>
 
 ---
 
---
+++ Java/submarine/equipment/sonar/HydrothermalVentMapper.java
@@ -0,0 +1,64 @@
+
+//////////////////////////////////////////////////////////////
+/////////////////////|      Day 05      |/////////////////////
+//////////////////////////////////////////////////////////////
+
+package submarine.equipment.sonar;
+
+import java.io.*;
+import java.util.regex.*;
+
+import submarine.core.*;
+
+public class HydrothermalVentMapper {
+    public static void main(String[] args) {
+        System.out.println(SimpleMap(DataTray.getInput(5)).dangerZones);
+    }
+    
+    public static Bathymetry SimpleMap(File file)
+    {
+        Bathymetry mapping = new Bathymetry(); 
+        for (String line : (new InputScannerString(file).getResult()))
+        {
+            Matcher match = Pattern.compile("(\\d*),(\\d*) -> (\\d*),(\\d*)").matcher(line); 
+            match.find();
+            int[] coord = new int[] { 
+                Integer.parseInt(match.group(1)), 
+                Integer.parseInt(match.group(2)), 
+                Integer.parseInt(match.group(3)), 
+                Integer.parseInt(match.group(4))};
+
+            if (coord[0] == coord[2] && coord[1] != coord[3])
+            {
+                mapping.add(coord[0],coord[1]);
+                int inc = coord[1] > coord[3] ? -1 : + 1;
+                while (coord[1] != coord[3])
+                {
+                    coord[1] += inc;
+                    mapping.add(coord[0],coord[1]);
+                }
+            }
+            else if (coord[0] != coord[2] && coord[1] == coord[3])
+            {
+                mapping.add(coord[0],coord[1]);
+                int inc = coord[0] > coord[2] ? -1 : + 1;
+                while (coord[0] != coord[2])
+                {
+                    coord[0] += inc;
+                    mapping.add(coord[0],coord[1]);
+                }
+            }
+        }
+        return mapping;
+    }
+}
+
+class Bathymetry
+{
+    public int[][] map = new int[1000][1000];
+    public int dangerZones = 0;
+    public void add(int x, int y)
+    {
+        if (++map[x][y] == 2) dangerZones++;
+    }
+}
