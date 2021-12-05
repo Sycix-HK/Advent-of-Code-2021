40eb3796492723eab8172e4fb40690d5a04a0462
Kristóf Hoffmann <WarriorKatHun@gmail.com>
2021-12-05 10:22:53 +0100

    Refactored day 2 into one file

--- Java/submarine/equipment/propeller/PathCalculation.java
+++ Java/submarine/equipment/propeller/PathCalculation.java
@@ -2,6 +2,7 @@ package submarine.equipment.propeller;
 
 import java.io.File;
 import java.util.*;
+import submarine.core.DataTray;
 
 enum Direction {
     forward,
@@ -9,6 +10,20 @@ enum Direction {
 }
 
 public class PathCalculation {
+
+    public static void main(String[] args) {
+        PathCalculation path = new PathCalculation(DataTray.getInput(2));
+        path.evaulateSimple();
+        System.out.println("Simple calculations: ");
+        System.out.println("Horizontal: "+ path.getHorizontal() + ", Depth: " + path.getDepth());
+        System.out.println("Horizontal * depth = " + path.getHorizontal() * path.getDepth());
+        path.resetPosition();
+        path.evaulateAdvanced();
+        System.out.println("Advanced calculations: ");
+        System.out.println("Horizontal: "+ path.getHorizontal() + ", Depth: " + path.getDepth());
+        System.out.println("Horizontal * depth = " + path.getHorizontal() * path.getDepth());
+    }
+
     private int depth = 0;
     private int horizontal = 0;
     private int aim = 0;
