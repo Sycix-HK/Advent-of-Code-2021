31c539f4eadc867617bc7e175ab98ce75308357c
Sycix-HK <warriorkathun@gmail.com>
2021-12-10 19:45:00 +0100

    timer + logger

--- Java/submarine/equipment/propeller/PathCalculation.java
+++ Java/submarine/equipment/propeller/PathCalculation.java
@@ -17,16 +17,13 @@ enum Direction {
 public class PathCalculation {
 
     public static void main(String[] args) {
+        TimeMeasure timer = new TimeMeasure();
         PathCalculation path = new PathCalculation(DataTray.getInput(2));
         path.evaulateSimple();
-        System.out.println("Simple calculations: ");
-        System.out.println("Horizontal: "+ path.getHorizontal() + ", Depth: " + path.getDepth());
-        System.out.println("Horizontal * depth = " + path.getHorizontal() * path.getDepth());
+        Logger.print(timer, "Simple distance", path.getHorizontal() * path.getDepth());
         path.resetPosition();
         path.evaulateAdvanced();
-        System.out.println("Advanced calculations: ");
-        System.out.println("Horizontal: "+ path.getHorizontal() + ", Depth: " + path.getDepth());
-        System.out.println("Horizontal * depth = " + path.getHorizontal() * path.getDepth());
+        Logger.print(timer, "Advanced distance", path.getHorizontal() * path.getDepth());
     }
 
     private int depth = 0;
