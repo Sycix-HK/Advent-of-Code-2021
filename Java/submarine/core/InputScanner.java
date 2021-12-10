0ecbd421dbcb5bf5e7528a6838b37de883489e27
Sycix-HK <warriorkathun@gmail.com>
2021-12-10 18:58:41 +0100

    Deleted integer scanner

--- Java/submarine/core/InputScanner.java
+++ Java/submarine/core/InputScanner.java
@@ -3,10 +3,10 @@ package submarine.core;
 import java.io.*;
 import java.util.*;
 
-public class InputScannerString {
+public class InputScanner {
     private ArrayList<String> result = new ArrayList<>();
 
-    public InputScannerString(File input){
+    public InputScanner(File input){
         try (Scanner in = new Scanner((new FileReader(input)))) {
             while (in.hasNext()) {
                 result.add(in.nextLine());
