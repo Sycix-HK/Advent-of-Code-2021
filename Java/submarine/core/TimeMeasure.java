8b482c9919cbfe1246092449c328a1494af77448
Sycix-HK <warriorkathun@gmail.com>
2021-12-10 18:59:16 +0100

    added segment measure

--- Java/submarine/core/TimeMeasure.java
+++ Java/submarine/core/TimeMeasure.java
@@ -2,13 +2,22 @@ package submarine.core;
 
 public class TimeMeasure {
     long start;
+    long segmentStart;
     public TimeMeasure()
     {
         start = System.currentTimeMillis();
+        segmentStart = System.currentTimeMillis();
     }
 
     public long elapsedTime()
     {
         return System.currentTimeMillis() - start;
+    }
+
+    public long elapsedSegment()
+    {
+        long outp = System.currentTimeMillis() - segmentStart;
+        segmentStart = System.currentTimeMillis();
+        return outp;
     }
 }
