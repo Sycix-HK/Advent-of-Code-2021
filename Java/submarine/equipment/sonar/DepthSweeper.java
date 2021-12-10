c2474885eb1fabb3f8d1a408bee503311d14f8ae
Sycix-HK <warriorkathun@gmail.com>
2021-12-10 19:01:51 +0100

    added timer and logger

--- Java/submarine/equipment/sonar/DepthSweeper.java
+++ Java/submarine/equipment/sonar/DepthSweeper.java
@@ -13,8 +13,9 @@ import java.util.stream.Collectors;
 public class DepthSweeper {
 
     public static void main(String[] args) {
-        System.out.println("Simple scan:    "+ sweepSimple  (DataTray.getInput(1)));
-        System.out.println("Denoised scan:  "+ sweepAdvanced(DataTray.getInput(1)));
+        TimeMeasure timer = new TimeMeasure();
+        Logger.print(timer, "Simple scan", sweepSimple(DataTray.getInput(1)));
+        Logger.print(timer, "Denoised scan", sweepAdvanced(DataTray.getInput(1)));
     }
 
     public static int sweepSimple(File f)
