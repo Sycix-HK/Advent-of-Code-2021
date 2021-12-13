0aaecb2a130737b5e6f019235f776c1bf5b126c7
Sycix-HK <warriorkathun@gmail.com>
2021-12-13 10:17:39 +0100

    Created the camera

---
+++ Java/submarine/equipment/camera/ThermalCamera.java
@@ -0,0 +1,8 @@
+package submarine.equipment.camera;
+
+public class ThermalCamera {
+    public static void main(String[] args) {
+        System.out.println("Congratulations on your purchase!");
+        System.out.println("To activate this infrared thermal imaging camera system, please enter the code found on page 1 of the manual.");
+    }
+}
