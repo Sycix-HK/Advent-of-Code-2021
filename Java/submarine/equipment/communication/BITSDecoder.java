cb6ccbacf3f32a9b3d204b620ddf1a16a71cf2e6
Sycix-HK <warriorkathun@gmail.com>
2021-12-27 14:25:42 +0100

    Solved day 16 part 1

--- Java/submarine/equipment/communication/BITSDecoder.java
+++ Java/submarine/equipment/communication/BITSDecoder.java
@@ -14,19 +14,17 @@ public class BITSDecoder
     public static void main(String[] args) 
     {
         TimeMeasure timer = new TimeMeasure();
-        Logger.print(timer, "Sum of version numbers", sumOfVersions(DataTray.getTest(16)));
+        Logger.print(timer, "Sum of version numbers", sumOfVersions(DataTray.getInput(16)));
     }
-
 
     public static int sumOfVersions(File file)
     {
         try (BufferedReader reader = new BufferedReader(new FileReader(file)))
         {
             Message m = new Message(reader.readLine());
-            return 5;
+            return m.main.versionSum();
         }
-        catch (Exception e) {Logger.error(e);}
-        return 2;
+        catch (Exception e) {Logger.error(e);return 0;}
     }
 }
 class Message
@@ -40,20 +38,13 @@ class Message
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < hex.length(); i++)
         {
-            sb.append(HexToBin(hex.charAt(i)));
+            sb.append(hexToBin(hex.charAt(i)));
         }
         bin = sb.toString();
         main = Packet.decode(bin);
-
-        for (int i = 0; i < bin.length(); i++)
-        {
-            int ver;
-            int type;
-            
-        }
     }
 
-    private static String HexToBin(char h)
+    private static String hexToBin(char h)
     {
         switch (h)
         {
@@ -73,43 +64,111 @@ class Message
             case 'D': return "1101";
             case 'E': return "1110";
             case 'F': return "1111";
-            default: throw new IllegalArgumentException("These elves are tripping");
+            default: throw new IllegalArgumentException("What the elves doing");
         }
     }
 }
 
 class Packet
 {
+    int len;
     int version;
-    int typeID;
-    public Packet(int version, int typeID)
-    {
-        this.version = version;
-        this.typeID = typeID;
-    }
+
     public static Packet decode(String bin)
     {
         int version = Integer.parseInt(bin.substring(0,3),2);
         int typeID = Integer.parseInt(bin.substring(3,6),2);
-        return typeID == 4 ? new Literal(version,typeID,bin) : new Operator(version,typeID,bin);
+        return typeID == 4 ? Literal.decode(version,bin) : Operator.decode(version,typeID,bin);
+    }
+    public int versionSum()
+    {
+        return version;
     }
 }
 
 class Literal extends Packet
 {
-    int value;
-    public Literal(int version, int typeID, String bin)
+    long value;
+    public static Literal decode(int version, String bin)
     {
-        super(version,typeID);
+        Literal outp = new Literal();
+        outp.version = version;
+        
+        StringBuilder builder = new StringBuilder();
+        int i = 6;
+        while (bin.charAt(i) == '1')
+        {
+            builder.append(bin.substring(i+1,i+5));
+            i+=5;
+        }
+        builder.append(bin.substring(i+1,i+5));
+        outp.value = Long.parseLong(builder.toString(),2);
+
+        outp.len = 6 + i-1;
+
+        return outp; 
     }
 }
 
 class Operator extends Packet
 {
+    int typeID;
     Packet[] packets;
-    public Operator(int version, int typeID, String bin)
+    public static Operator decode(int version, int typeID, String bin)
     {
-        super(version,typeID);
+        return bin.charAt(6) == '0' ? fifteenBit(version, typeID, bin) : elevenBit(version, typeID, bin);
+    }
+    public static Operator fifteenBit(int version, int typeID, String bin)
+    {
+        Operator outp = new Operator();
+        outp.version = version;
+        outp.typeID = typeID;
+        List<Packet> packetlist = new ArrayList<>();
+
+        int packetslen = Integer.parseInt(bin.substring(7,7+15),2);
+        int packetcursor = 0;
+        while (packetcursor < packetslen)
+        {
+            Packet p = Packet.decode(bin.substring(packetcursor+7+15));
+            packetcursor += p.len;
+            packetlist.add(p);
+        }
+
+        outp.packets = packetlist.toArray(new Packet[0]);
+        outp.len = 7 + 15 + packetslen;
+
+        return outp;
+    }
+
+    public static Operator elevenBit(int version, int typeID, String bin)
+    {
+        Operator outp = new Operator();
+        outp.version = version;
+        outp.typeID = typeID;
+
+        int packetcount = Integer.parseInt(bin.substring(7,7+11),2);
+        int packetcursor = 0;
+        outp.packets = new Packet[packetcount];
+        for (int i = 0; i < packetcount; i++)
+        {
+            outp.packets[i] = Packet.decode(bin.substring(packetcursor+7+11));
+            packetcursor += outp.packets[i].len;
+        }
+
+        outp.len = 7 + 11 + packetcursor;
+        
+        return outp;
+    }
+
+    @Override
+    public int versionSum()
+    {
+        int sum = 0;
+        for (Packet packet : packets) {
+            sum += packet.versionSum();
+        }
+        return sum + version;
     }
 }
+
 
